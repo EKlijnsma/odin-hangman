36910c89d9bba996571a201f6fcdb7e5462a3770
Emiel Mac <112609864+EKlijnsma@users.noreply.github.com>
2024-07-12 15:31:46 +0200

    add JSON serialization

--- lib/secret_word.rb
+++ lib/secret_word.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'json'
+
 # This class contains the secret word for hangman in the @secret attribute
 # It also contains a @display attribute that shows the word in underscores
 # For every letter that is guessed correctly the letter will be filled in to the @display attribute
@@ -30,6 +32,21 @@ class SecretWord
 
   def win?
     @secret == @display
+  end
+
+  def to_json(*_args)
+    JSON.dump({
+                secret: @secret,
+                display: @display
+              })
+  end
+
+  def self.from_json(string)
+    data = JSON.parse(string)
+    instance = allocate
+    instance.instance_variable_set(:@secret, data['secret'])
+    instance.instance_variable_set(:@display, data['display'])
+    instance
   end
 
   attr_reader :secret, :display
